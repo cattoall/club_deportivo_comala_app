984a725ac7ca1d8390c50f1ee597205c490a4655
cattoall <cat_to_all@hotmail.com>
2021-07-01 10:33:16 -0500

    Agregar BottomNavigatorBar en mainManu

--- lib/mainMenu.dart
+++ lib/mainMenu.dart
@@ -6,36 +6,87 @@ class MainMenu extends StatefulWidget {
 }
 
 class _State extends State<MainMenu> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Consulta de Inventario',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Consulta de Inventario Consolidado',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Ingreso de Factura',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Consulta de Clientes',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Corte de Caja',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 5: Usuario',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 7,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.inventory_2_outlined)),
-              Tab(icon: Icon(Icons.inventory_outlined)),
-              Tab(icon: Icon(Icons.ballot_outlined)),
-              Tab(icon: Icon(Icons.my_library_books_outlined)),
-              Tab(icon: Icon(Icons.people_alt_outlined)),
-              Tab(icon: Icon(Icons.attach_money_outlined)),
-              Tab(icon: Icon(Icons.perm_identity)),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Equilibrium POS')),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inventory_2_outlined),
+            label: 'Inventario',
+            backgroundColor: Colors.blueGrey,
           ),
-          title: Center(child: Text('Equilibrium POS')),
-        ),
-        body: TabBarView(
-          children: [
-            Icon(Icons.inventory_2_outlined),
-            Icon(Icons.inventory_outlined),
-            Icon(Icons.ballot_outlined),
-            Icon(Icons.my_library_books_outlined),
-            Icon(Icons.people_alt_outlined),
-            Icon(Icons.attach_money_outlined),
-            Icon(Icons.perm_identity),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inventory_outlined),
+            label: 'Consolidado',
+            backgroundColor: Colors.blueGrey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.my_library_books_outlined),
+            label: 'Factura',
+            backgroundColor: Colors.blueGrey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt_outlined),
+            label: 'Clientes',
+            backgroundColor: Colors.blueGrey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money_outlined),
+            label: 'Caja',
+            backgroundColor: Colors.blueGrey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_identity),
+            label: 'Usuario',
+            backgroundColor: Colors.blueGrey,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white70,
+        onTap: _onItemTapped,
       ),
     );
   }
